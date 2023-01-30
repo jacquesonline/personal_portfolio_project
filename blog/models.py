00dab44838e2e6eb9ed565a54278642e69a0982b
EC2 Default User <ec2-user@ip-10-0-25-18.ap-southeast-2.compute.internal>
2023-01-30 00:40:48 +0000

    First commit

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=25)
+    date = models.DateField()
+    description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title
