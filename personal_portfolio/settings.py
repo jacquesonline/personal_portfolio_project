8f16c0bcbc183785424af96ea978115fd7648020
Your Name <you@example.com>
2023-01-30 19:00:27 +1100

    local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -27,7 +27,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['jslearns.pythonanywhere.com']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -126,3 +125,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like you are in production")
